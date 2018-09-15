c01ab535beec372e5a72653a46ecbaaff0267e15
arxcdr <34399111+arxcdr@users.noreply.github.com>
2018-09-15 13:24:12 +0200

    Add project files

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Ball : MonoBehaviour {
+
+    // Config params
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
+    [SerializeField] AudioClip[] ballSounds;
+    [SerializeField] float randomFactor = 0.2f;
+    bool hasStarted = false;
+
+    // State
+    Vector2 paddleToBallVector;
+
+    // Cached component references
+    AudioSource myAudioSource;
+    Rigidbody2D myRigidBody2D;
+
+	// Use this for initialization
+	void Start () {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+
+        // This line is to address Unity glitch that makes dynamic bodies slowly go down for no reason
+        GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+
+        myAudioSource = GetComponent<AudioSource>();
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }      
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector2 velocityTweak = new Vector2(UnityEngine.Random.Range(0f, randomFactor),
+                                            UnityEngine.Random.Range(0f, randomFactor));
+        if (hasStarted)
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
+            myRigidBody2D.velocity += velocityTweak;
+        }        
+    }
+}
